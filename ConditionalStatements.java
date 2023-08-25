c16d7018689d043166b2c79ce5d140a523423234
SureshSasikumar <118600976+SureshSasikumar@users.noreply.github.com>
2023-08-25 18:31:04 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		
+		int number =35;
+		
+		//type if -> ctrl +space ->enter
+		
+		
+		if (number>0) {
+			
+			System.out.println("the given number:" + number + "is positive");
+		}
+		else if (number==0) {
+			System.out.println("the given number:" + number + "is neutral");
+		}
+		else {
+				System.out.println("The number is neither positive nor negative");
+				
+
+			
+		}
+		
+		
+	}
+}
---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		int date=19;
+		boolean reached =true;
+		String name ="Suresh";
+		
+		System.out.println("date is:" + date + '\n' + "name is:" +name + '\n'+ "are you reached:" +reached );
+					
+				
+				
+	}
+
+}
